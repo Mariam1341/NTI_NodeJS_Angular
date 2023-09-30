5b1722ee5fe7de94f5423dc71b14fa36ada240e6
Mariam1341 <mmm01061480913@gmail.com>
2023-09-30 19:53:51 +0300

    backend

---
+++ Final _Project/Backend/models/user_model.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const USER_SCHEMA = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+    default: "https://i.imgur.com/4s5qLzU.png",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address:{
+    type:String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  // favoritemedicines: [
+  //   {
+  //     medicineId:{
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       ref: 'medicine'
+  //     },
+  //     medicineTitle:{
+  //       type: String,
+  //       required: true,
+  //     },
+  //     medicineImage:{
+  //       type: String,
+  //       required: true,
+  //     },
+  //     medicinePrice:{
+  //       type: Number,
+  //       required: true,
+  //     }
+  //   },
+  // ],
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+// Encrypting password before save
+USER_SCHEMA.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+USER_SCHEMA.pre('remove',async function(next){
+  try{
+    await this.model('Cart').deleteOne({'userId':this._id});
+  }catch(e){
+    console.log(e);
+  }
+});
+
+// Return JWT token
+USER_SCHEMA.methods.getJwtToken = async function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+
+};
+
+
+USER_SCHEMA.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const USER = mongoose.model("User", USER_SCHEMA);
+
+module.exports = USER;
